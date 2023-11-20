9857c5067386b7f209d353480bbd404a62d5751b
Javi <javiperezperez265@gmail.com>
2023-11-20 18:33:37 +0000

    Códgio correcto del ejercicio 3

---
+++ PruebaCorrecta3.java
@@ -0,0 +1,17 @@
+public class PruebaCorrecta3 {
+    public static void mostrartablasparcialmente(int a, int b){
+        int valor;
+        for (int i=1; i<=a; i++){
+        for (int j=1; j<=a;j++){
+            valor = i * j;
+            System.out.println(i + " * " + j + " = "+ (valor));
+        }
+            System.out.println("--------");
+        }
+    }
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 2;
+        mostrartablasparcialmente(x, y);
+    }
+}
