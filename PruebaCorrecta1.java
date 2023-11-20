f6fd2ec4769ea4e7733d17637add8ee7c7662d5f
Javi <javiperezperez265@gmail.com>
2023-11-20 18:29:35 +0000

    Codigo Solucionado del ejercicio 1

---
+++ PruebaCorrecta1.java
@@ -0,0 +1,14 @@
+public class PruebaCorrecta1 {
+public static void calculatabla(int a){
+int valor;
+for (int i=1; i<=10;i++){
+valor = a * i;
+System.out.println(a + " x " + i + " = " + valor);
+}
+}
+public static void main(String[] args) {
+int x=2;
+calculatabla(x);
+}
+}
+
